37c24770fa4b9e7e0f953c91aa7f830b77fe4b79
M-Vamshi <21211a05f1@bvrit.ac.in>
2024-03-10 16:18:31 +0530

    checking workflow

--- src/utilities/userFamilyAdminCheck.ts
+++ src/utilities/userFamilyAdminCheck.ts
@@ -1,10 +1,11 @@
 import { Types } from "mongoose";
-import { errors, requestContext } from "../libraries";
-import { USER_NOT_AUTHORIZED_ADMIN } from "../constants";
-import type { InterfaceUserFamily } from "../models/userFamily";
-import { User } from "../models";
+import { UnauthorizedError } from "../libraries/errors/unauthorizedError.js";
+import {translate} from "../libraries/requestContext.js"
+import { USER_NOT_AUTHORIZED_ADMIN } from "../constants.js";
+import type { InterfaceUserFamily } from "../models/userFamily.js";
+import { User } from "../models/User.js";
 /**
- * If the current user is an admin of the organisation, this function returns `true` otherwise it returns `false`.
+ * If the current user is an admin of the organization, this function returns `true` otherwise it returns `false`.
  * @remarks
  * This is a utility method.
  * @param userId - Current user id.
@@ -27,8 +28,8 @@ export const adminCheck = async (
     : false;
 
   if (!userIsUserFamilyAdmin && !isUserSuperAdmin) {
-    throw new errors.UnauthorizedError(
-      requestContext.translate(`${USER_NOT_AUTHORIZED_ADMIN.MESSAGE}`),
+    throw new UnauthorizedError(
+      translate(`${USER_NOT_AUTHORIZED_ADMIN.MESSAGE}`),
       USER_NOT_AUTHORIZED_ADMIN.CODE,
       USER_NOT_AUTHORIZED_ADMIN.PARAM,
     );
